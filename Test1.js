94a839c82e636aad58c4f733768a60e8acf6c5c3
wohpe <83101710+wohpe@users.noreply.github.com>
2021-08-30 13:02:12 +0900

    Update Test1.js

--- Test1.js
+++ Test1.js
@@ -1,47 +1,56 @@
 const { Extension, Block } = EntLib
 
-const extension = new Extension({
-    displayName: '확장블록',
+const print_None = new Extension({
+    displayName: 'print_None',
     blocks: {
         logToConsole: new Block({
-            template: '[콘솔에 (text: Hello world!) 출력하기]',
+            template: '[엔트리 콘솔에 (text: Test) 출력하기]',
             action({ text }) {
-                console.log(text)
+               Entry.console.print(text)
             }
         })
     }
 })
 
-extension.enable()
-Extension.allExtensionsLoaded()
-
-// When Using Multiple Extensions
-
-const extension1 = new Extension({
-    displayName: '확장블록',
-    blocks: {
-        logToConsole: new Block({
-            template: '[콘솔에 (text: Hello world!) 출력하기]',
-            action({ text }) {
-                console.log(text)
-            }
-        })
-    }
-})
-
-const extension2 = new Extension({
-    displayName: '확장블록2',
+const print_ask = new Extension({
+    displayName: 'print_ask',
     blocks: {
         logToConsole2: new Block({
-            template: '[콘솔에 (text: Hello world!) 출력하기]',
+            template: '[엔트리 콘솔에 (text: Test) 출력하기]',
             action({ text }) {
-                console.log(text)
+              Entry.console.print(text,"ask") // 질문
             }
         })
     }
 })
 
-extension1.enable()
-extension2.enable()
+const print_speak = new Extension({
+    displayName: 'print_speak',
+    blocks: {
+        logToConsole3: new Block({
+            template: '[엔트리 콘솔에 (text: Test) 출력하기]',
+            action({ text }) {
+              Entry.console.print(text,"speak")
+            }
+        })
+    }
+})
+
+// const extension2 = new Extension({
+//     displayName: '확장블록2',
+//     blocks: {
+//         logToConsole2: new Block({
+//             template: '[콘솔에 (text: Hello world!) 출력하기]',
+//             action({ text }) {
+//                 console.log(text)
+//             }
+//         })
+//     }
+// })
+
+print_None.enable()
+print_ask.enable()
+print_speak.enable()
+// extension2.enable()
 
 Extension.allExtensionsLoaded() // Must call this when all extensions enabled
